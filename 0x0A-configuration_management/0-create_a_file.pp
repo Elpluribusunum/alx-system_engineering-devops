31764e5ceea3a298ca3bee4735e9dfa271ae82aa
Elpluribusunum <iyogunclarkson@gmail.com>
2023-09-22 20:17:24 -0700

    woow

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
+#The code will create a file name school inside the /tmp directory
 file { '/tmp/school':
-ensure   =>  file,
-content  =>  'I love Puppet',
-mode     =>  '07744',
-owner    =>  'www-data',
-group    =>  'www-data'
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
