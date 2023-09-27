ed15cee7cf07c4d4548140c8f3e19ddb75faef98
Elpluribusunum <iyogunclarkson@gmail.com>
2023-09-26 17:22:24 -0700

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#!usr/bin/env bash
+# Making changes to our configuration file with puppet
+
+file { 'ect/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+}
